e9675667a76dace965fe8b04b81e0ddf5f984b51
cristian-franco <cristianfranco.cf@gmail.com>
2018-09-04 21:34:40 -0700

    set up the basic layout for the application

--- FirstPersonalProject/mainwindow.cpp
+++ FirstPersonalProject/mainwindow.cpp
@@ -6,6 +6,34 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QWidget* buttonWidget = new QWidget;
+    QVBoxLayout* layoutVert = new QVBoxLayout;
+    QPushButton* buttonRandom = new QPushButton("Random");
+    QPushButton* buttonDwarf = new QPushButton("Dwarf");
+    layoutVert->addWidget(buttonRandom);
+    layoutVert->addWidget(buttonDwarf);
+    buttonWidget->setLayout(layoutVert);
+
+    QTextEdit* statDisplay = new QTextEdit;
+
+
+
+    QWidget* mainWidget = new QWidget;
+    QHBoxLayout* layoutHoriz = new QHBoxLayout;
+    layoutHoriz->addWidget(buttonWidget);
+
+    layoutHoriz->addWidget(statDisplay);
+
+    mainWidget->setLayout(layoutHoriz);
+
+    setCentralWidget(mainWidget);
+
+
+
+
+
+
 }
 
 MainWindow::~MainWindow()
--- FirstPersonalProject/mainwindow.h
+++ FirstPersonalProject/mainwindow.h
@@ -2,21 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QPushButton>
+#include <QTextEdit>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    private:
+        Ui::MainWindow *ui;
+        QWidget* mainWidget;
+        QWidget* buttonWidget;
+        QHBoxLayout* layoutHoriz;
+        QVBoxLayout* layoutVert;
+        QPushButton* buttonRandom;
+        QTextEdit* statDisplay;
+
+
+
 };
 
 #endif // MAINWINDOW_H
